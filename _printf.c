b221ae9951478ab4c43298ec0e01809f4ba4854e
ziad464 <zezozeyad7654@gmail.com>
2023-04-23 04:01:53 -0700

    C - printf

--- _printf.c
+++ _printf.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
